e05d3c80e61909833c7019b510b7a35a48ad8295
Mark Lopez <m@silvenga.com>
2017-10-26 19:18:11 -0500

    Add project files.

---
+++ DnsCompose/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DnsCompose
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
